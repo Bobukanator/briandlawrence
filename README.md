b29f04e4a5a12cd7932da52fe25bc22be22a295d
Brian Lawrence <Bobukanator@gmail.com>
2022-03-30 15:35:32 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# briandlawrence
+
+Personal website for Brian David Lawrence
+Feel free to reach out with questions: Brian@BrianDLawrence.com 
+
+## Build Setup
+
+```bash
+# install dependencies
+$ npm install
+
+# serve with hot reload at localhost:3000
+$ npm run dev
+
+# build for production and launch server
+$ npm run build
+$ npm run start
+
+# generate static project
+$ npm run generate
+```
+
+For detailed explanation on how things work, check out the [documentation](https://nuxtjs.org).
+
+
---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="section">
+    <div class="columns is-mobile">
+      <card title="Technology" icon="github">
+        See my code on <a href="https://github.com/Bobukanator"> GitHub </a>
+      </card>
+
+      <card title="Linkedin" icon="cellphone-link">
+        Profile on
+        <a href="https://www.linkedin.com/in/briandlawrence/"> LinkedIn </a>
+      </card>
+    </div>
+  </section>
+</template>
+
+<script>
+import Card from "~/components/Card";
+
+export default {
+  name: "IndexPage",
+  components: {
+    Card,
+  },
+};
+</script>
