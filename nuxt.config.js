1297aec5d0140157de37f8b99a6e99ee707613c0
Brian Lawrence <Bobukanator@gmail.com>
2022-04-05 14:02:50 +0200

    Added Google Analytics

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,7 +28,9 @@ export default {
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
+    '@nuxtjs/google-analytics'
   ],
+
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
@@ -38,5 +40,8 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+  },
+  googleAnalytics: {
+    id: 'UA-18727748-2'
   }
 }
