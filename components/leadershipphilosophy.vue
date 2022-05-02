68d0464c772ff575e1970b995584a1bb3379d976
Brian Lawrence <Bobukanator@gmail.com>
2022-05-02 16:33:10 +0200

    Updated Leadership section

---
+++ components/leadershipphilosophy.vue
@@ -0,0 +1,135 @@
+<template>
+  <div class="box">
+    <h1 class="title has-text-centered">
+      Brian Lawrence’s Leadership Philosophy
+    </h1>
+    <div class="content">
+      <p>
+        If you ask 100 people what makes a great leader, you will get 100
+        different answers. What it means to be a leader and what makes a great
+        leader is subjective however some traits and attributes stand out above
+        others. I have spent much of my career attempting to learn what makes a
+        genuine leader. I have learned from mentors, leadership books, and from
+        my own mistakes.
+      </p>
+      <p>
+        This leadership philosophy was created to help articulate what my
+        definition of a great leader is. The benefit of having this written on
+        paper is to remind myself so that I can walk the walk while informing
+        those that work with me.
+      </p>
+      <p>
+        The most important leadership attributes that govern me as a leader are
+        trust, purpose, and passion. Trust within a team is the glue that holds
+        the team together. Having purpose provides the fuel that enables
+        satisfaction in your work. Passion is the hard work that creates
+        success.
+      </p>
+    </div>
+    <h2 class="subtitle">
+      The following are the commitments I make to my fellow associates using
+      these attributes as a guide:
+    </h2>
+    <div class="content">
+      <b>Trust</b>
+      <ul>
+        <li>I will try to set expectations for what I expect from you.</li>
+        <li>I will do my best to understand your expectations of me.</li>
+        <li>
+          I will take 100% responsibility for my action and my team's actions.
+        </li>
+        <li>I will be transparent with decisions.</li>
+      </ul>
+      <b>Purpose</b>
+      <ul>
+        <li>
+          Purpose is the reason for why something exists. I commit to helping
+          associates understand the company purpose.
+        </li>
+        <li>
+          I commit to following my purpose and helping others define and
+          understand theirs.
+        </li>
+      </ul>
+      <b>Passion</b>
+      <ul>
+        <li>
+          Passion is the hard work, but hard work without direction and control
+          is not sufficient which is why I view time as a vital resource; it is
+          critical to focus your hard work and time on the right things. I will
+          do my best to evaluate where I am spending my time to be as efficient
+          as possible.
+        </li>
+        <li>
+          I will do whatever I can do to remove barriers to our progress as a
+          team and your progress towards your goals and personal development.
+        </li>
+      </ul>
+    </div>
+    <h2 class="subtitle">
+      With these attributes as a guide, the following are expectations I have
+      for associates that work on my team.
+    </h2>
+    <div class="content">
+      <b>Trust</b>
+      <ul>
+        <li>Do your best to understand my expectations of you.</li>
+        <li>Do your best to set expectations for me.</li>
+        <li>Take 100% responsibility for your actions, don’t blame others.</li>
+      </ul>
+      <b>Purpose</b>
+      <ul>
+        <li>
+          Do your best to understand the company and team purpose; please ask me
+          questions if you do not.
+        </li>
+        <li>
+          Have a purpose for yourself. If you don’t have one, talk to me, I love
+          helping people grow in this area.
+        </li>
+      </ul>
+      <b>Passion</b>
+      <ul>
+        <li>
+          Spend your time on the right things. If you aren’t sure what those
+          are, please work with me to help figure that out.
+        </li>
+        <li>
+          Continue to learn - Tim Ferris once said that the opposite of
+          happiness is not sadness, it is boredom. If you continue to learn, you
+          will never be bored and always be happy plus you will improve yourself
+          and as a result your life and our team!
+        </li>
+      </ul>
+      <p>
+        A mentor of mine once told me that authentic leaders create more
+        leaders, I embrace this perspective and strive to create more leaders
+        here at Cerner. I will continue to improve this philosophy and my self
+        by learning through every avenue which may include asking for feedback,
+        reading, taking courses, and learning from experiencing.
+      </p>
+    </div>
+    <h2 class="subtitle">Memorable Leadership Quotes</h2>
+    <div class="content">
+      <p>
+        “Pick your associates well, back them fully, empower them with both
+        accountability and responsibility, and they will produce far more than
+        you ever will achieve on your own.” ~Michael Useem
+      </p>
+      <p>
+        “Do not wait to be told what to do. That is a disaster as a leader.”
+        ~Bill Mahoney
+      </p>
+      <p>
+        “If you invest all available time in optimizing the initial plan, you
+        never start.” ~Neil Patterson
+      </p>
+      <p>
+        “I’ve learned that people will forget what you said, people will forget
+        what you did, but people will never forget how you made them feel.”
+        ~Mary Angelou
+      </p>
+      <p>“Skills are cheap. Passion is priceless” ~ Gary Vaynerchuk</p>
+    </div>
+  </div>
+</template>
--- pages/leadership.vue
+++ pages/leadership.vue
@@ -1,8 +1,38 @@
 <template>
-  <brianleadership></brianleadership>
+  <section>
+    <div class="content">
+      <p>
+        I have led various organizations and initiatives for over two decades,
+        both professionally and personally. I have found that the best leaders
+        are like the best parents. They give their teams the time each
+        individual requires to grow and be productive.
+      </p>
+      <p>
+        I have managed various roles, including software engineers, user
+        experience designers & researchers, project managers, system analysts,
+        and design managers.
+      </p>
+      <p>
+        Career coaching and the development of others are one of my passions.
+        Early in my career, I had the opportunity to be a founding member of a
+        young professional employee resource group. As part of my
+        responsibilities, I drove career development programs. Much of what I
+        learned through those programs, I have been able to bring to my
+        management style.
+      </p>
+      <p>
+        I believe that leadership is a skill that you can learn. Therefore,
+        besides learning by experience, I relied on mentors and read as much as
+        possible on the topic. A book I read called the Leader's Compass
+        introduced the concept of a Leadership Philosophy, which you can read
+        below:
+      </p>
+    </div>
+    <brianleadership></brianleadership>
+  </section>
 </template>
 <script>
-import brianleadership from "../components/brianleadership.vue";
+import brianleadership from "../components/leadershipphilosophy.vue";
 
 export default {
   name: "Leadership",
