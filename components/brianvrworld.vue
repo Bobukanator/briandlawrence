f1938d3e998d267ff5969767efd1f317b20f67b8
Brian Lawrence <Bobukanator@gmail.com>
2022-05-11 15:09:29 +0200

    updated VR - added walls and skypicture

--- components/brianvrworld.vue
+++ components/brianvrworld.vue
@@ -1,27 +1,70 @@
 <template>
   <div class="content">
     <a-scene physics>
+
+      <img id="city" src="images/henning-witzel-unsplash.jpg">
+      <img id="wall" src="images/vintagewoodwall.jpg">
+      
       <a-box
         position="-1 4 -3"
         rotation="0 45 0"
         color="green"
         dynamic-body
       ></a-box>
+      
       <a-plane
+        id="floor"
         position="0 1 -4"
         rotation="-90 0 0"
-        width="4"
-        height="4"
-        color="darkseagreen"
+        width="40"
+        height="40"
+        color="gray"
         static-body
       ></a-plane>
+      <a-plane
+        id="ceiling"
+        position="0 6 -4"
+        rotation="90 0 0"
+        width="40"
+        height="40"
+        color="blue"
+        static-body
+      ></a-plane>
+
       <a-entity
         gltf-model="BLawPixelPerson.glb"
-        position="1.5 .5 -4.5"
+        position="1.5 0.4 -4.5"
         rotation="-90 180 0"
         scale=".3 .3 .3"
       ></a-entity>
-      <a-sky color="#ECECEC"></a-sky>
+
+       <a-sky id="image-360" radius="100" src="#city"></a-sky>
+       <a-box id="northwall1" 
+        position="-5 3 -5" 
+        scale="10 5 .1"
+        rotation="0 0 0"
+        src="#wall">
+        </a-box>
+        <a-box id="northwall2" 
+        position="5 3 -5" 
+        scale="10 5 .1"
+        rotation="0 0 0"
+        src="#wall">
+        </a-box>
+        <a-box id="eastwall1" 
+        position="10 3 0" 
+        scale="10 5 .1"
+        rotation="0 90 0"
+        src="#wall">
+        </a-box>
+        <a-box id="westwall1" 
+        position="-10 3 0" 
+        scale="10 5 .1"
+        rotation="0 90 0"
+        src="#wall">
+        </a-box>
+
+  
     </a-scene>
   </div>
 </template>
