32bf78ae49143549303aca59fe3dd05eb27459b8
Brian Lawrence <Bobukanator@gmail.com>
2022-06-30 20:06:22 +0200

    Updated entrepreneur

---
+++ components/brianprojects.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="box">
+    <h1 class="title has-text-centered">Brian's Projects (Fail Resume!)</h1>
+    <article class="media">
+      <div class="media-left">
+        <figure class="image is-64x64">
+          <img src="images/youtube_social_square_white.png" alt="Image" />
+        </figure>
+      </div>
+      <div class="media-content">
+        <div class="content">
+          <p>
+            <strong>2007-present YouTube</strong>
+            <small
+              ><a href="https://www.youtube.com/c/BrianLawrence1"
+                >Personal Channel</a
+              >
+            </small>
+            <br />
+            I joined YouTube in 2007 and uploaded my first video. After reading
+            Gary V's book, "Crush It," I decided to try to become an influencer.
+            I started posting videos about my travel, my speeches (once I joined
+            Toastmasters), and some software coding talks. One day, I posted a
+            fun video I made of a person in a dinosaur costume at a local radio
+            station event. That video went viral with over 2 million views, this
+            helped boost visits & subscribers to my channel. I still have this
+            channel today and recently have posted about Spain, UX design, and
+            the occasional cooking video. I have not become a true influencer,
+            BUT I love making and posting videos.
+          </p>
+        </div>
+        <nav class="level is-primary">
+          <div class="level-left">
+            <a class="level-item" aria-label="reply">
+              <span class="icon is-small">
+                <i class="fas fa-reply"></i>
+              </span>
+            </a>
+            <a class="level-item" aria-label="retweet">
+              <span class="icon is-small">
+                <i class="fas fa-retweet"></i>
+              </span>
+            </a>
+            <a class="level-item" aria-label="like">
+              <span class="icon is-small">
+                <i class="fas fa-heart"></i>
+              </span>
+            </a>
+          </div>
+        </nav>
+      </div>
+    </article>
+    <article class="media">
+      <div class="media-left">
+        <figure class="image is-64x64">
+          <img src="images/SsZ_LightWP4x4_2.png" alt="Image" />
+        </figure>
+      </div>
+      <div class="media-content">
+        <div class="content">
+          <p>
+            <strong>2010-2011 YPCareer-SSZ.TV</strong>
+            <small>Professional Vlog/Blog</small>
+            <br />
+            As I progressed in my career at Siemens, I realized that much of
+            what I was learning should be shared. So, I started a blog called
+            Young Professional Career (YPCareer.com). In addition, I decided to
+            use a multi-channel marketing approach and created a video blog
+            using the same content called SolutionShortz aka SSZ.TV. Over
+            several years, I created around 40 articles, some of which were also
+            turned into a youtube video.
+          </p>
+        </div>
+      </div>
+    </article>
+    <article class="media">
+      <div class="media-left">
+        <figure class="image is-64x64">
+          <img src="images/SF_LogoDraft1.jpg" alt="Image" />
+        </figure>
+      </div>
+      <div class="media-content">
+        <div class="content">
+          <p>
+            <strong>2011-2012 YSMR-SocialFolio</strong>
+            <small>Smart Website</small>
+            <br />
+            One of my favorite blog posts, "The Traditional Resume is Quickly
+            Becoming Obsolete," led me to start a new project called Your Social
+            Media Resume (transitioning to Social Folio). The idea was to create
+            a website that would scan your social media presence and provide a
+            score along with guidance on how to fix and or improve your social
+            media footprint! This project was the first time I created a team to
+            help. I took the concept to some of my friends at Drexel University
+            who were still students and pitched the idea. I had a rough
+            prototype created and sold it. We then started to produce a better
+            version using Java instead of PHP WordPress plugin code. This
+            project eventually fell apart as we could not commit as a team with
+            our busy schedules! It was fun, though, and I still think it could
+            have been a fantastic product if only we were more focused!
+          </p>
+        </div>
+      </div>
+    </article>
+  </div>
+</template>
--- pages/entrepreneur.vue
+++ pages/entrepreneur.vue
@@ -1,7 +1,29 @@
 <template>
-  <div>Entrepreneur</div>
+  <section>
+    <div class="content">
+      <p>
+        In 2008, I read some
+        <a href="https://www.pinterest.com/blawrence57/life-changing-books/"
+          >fantastic books</a
+        >
+        like Think and Grow Rich, The 4-hour workweek, and Rich Dad - Poor Dad.
+        These books gave me the confidence and drive to pursue entrepreneurial
+        activities on my nights and weekends.
+      </p>
+      <p>
+        The following are various entrepreneurial like projects I have worked on
+        over the years. I haven't had financial success yet; however, I have
+        enjoyed the time I have spent and the things I have learned because of
+        these activities. I suspect that I will always be involved in some type
+        of fun project throughout my life! None of these are on my official
+        Linkedin (maybe someday I will move them).
+      </p>
+    </div>
+    <brianprojects></brianprojects>
+  </section>
 </template>
 <script>
+import brianprojects from "../components/brianprojects.vue";
 export default {
   name: "Entrepreneur",
   head() {
@@ -17,5 +39,6 @@ export default {
       ],
     };
   },
+  components: { brianprojects },
 };
 </script>
